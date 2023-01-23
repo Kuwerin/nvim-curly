41dd3fd92d1aaa8cf06904c81a0d5b749f87b226
Simon Kuwerin <kuwerin@gmail.com>
2023-01-23 19:02:08 +0300

    Add navigation methods

--- lua/nvim-curly/filestructure.lua
+++ lua/nvim-curly/filestructure.lua
@@ -1,6 +1,6 @@
 local M = {}
 
--- Gets workspace location.
+-- Gets workspace location
 function M.get_workspace()
   return vim.g.nvim_curly_root_dir or "~/.vim/curl/"
 end
@@ -8,6 +8,14 @@ end
 -- Resolves filename
 function M.get_filename()
   return "history/"..os.date("%d-%m-%y_%H:%M:%S")..".json"
+end
+
+function M.go_to_workspace_root()
+    vim.cmd(":e ", M.get_workspace())
+end
+
+function M.go_to_history()
+    vim.cmd(":e ", M.get_workspace(), "history/")
 end
 
 return M
