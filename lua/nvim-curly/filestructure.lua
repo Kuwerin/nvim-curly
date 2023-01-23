d846dbdee6bf3c974f9e287718bbda6514c1912f
Simon Kuwerin <kuwerin@gmail.com>
2023-01-23 19:09:08 +0300

    Fix concatenation error

--- lua/nvim-curly/filestructure.lua
+++ lua/nvim-curly/filestructure.lua
@@ -26,11 +26,11 @@ function M.go_to_history()
 end
 
 function M.go_to_workspace_root()
-    vim.cmd(":e ", M.get_workspace())
+    vim.cmd(":e "..M.get_workspace())
 end
 
 function M.go_to_history()
-    vim.cmd(":e ", M.get_workspace(), "history/")
+    vim.cmd(":e "..M.get_workspace(), "history/")
 end
 
 return M
